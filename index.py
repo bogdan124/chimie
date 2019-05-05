691a98d3bc47aa7f67bab6e3c2adacdb6fb3ce22
Bogdan123 <35634065+bogdan124@users.noreply.github.com>
2019-05-05 04:41:27 -0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,28 @@
+from flask import Flask ,render_template,jsonify
+
+
+app=Flask(__name__)
+
+@app.route('/',methods=['POST','GET'])
+def index():
+    return render_template("index.html")
+
+@app.route('/simulation',methods=['POST','GET'])
+def simulation():
+    return render_template("simulation.html")
+
+@app.route('/home',methods=['POST','GET'])
+def home():
+    return render_template("home.html")
+
+@app.route('/importanta',methods=['POST','GET'])
+def importanta():
+    return render_template("importanta.html")
+
+
+if __name__=='__main__':
+    app.run(debug=True,port=5444)
+
+
+
+
